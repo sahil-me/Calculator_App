f772418453bd56ac87d58afcdd6470d57e83ed7e
Sahil Sharma <sharmasahil201912@gmail.com>
2025-03-30 18:05:56 +0530

    Restoring full project

--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+import gui.CalculatorGui;
 
 public class CalculatorApp {
+    public static void main(String[] args){
+        new CalculatorGui().setVisible(true);
+    }
 }
